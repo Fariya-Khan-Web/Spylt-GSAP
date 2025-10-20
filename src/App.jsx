8e6551ecb4a825ed012c20ca06ac92c2f7c44a0f
Fariya <fariya.webdev@gmail.com>
2025-10-20 18:22:42 +0600

    GSAP: Hero animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import { ScrollTrigger } from "gsap/all"
 import Navbar from "./Components/Navbar"
 import Hero from "./Sections/Hero"
+import gsap from "gsap"
+
+gsap.registerPlugin(ScrollTrigger)
 
 
 function App() {
@@ -8,6 +12,7 @@ function App() {
     <>
       <Navbar/>
       <Hero/>
+      <div className="min-h-dvh z-20 bg-red"></div>
     </>
   )
 }
--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,6 +1,56 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { SplitText } from 'gsap/all';
 import React from 'react';
 
 const Hero = () => {
+
+    useGSAP(() => {
+        const titleSplit = SplitText.create('.hero-title', {
+            type: 'chars'
+        })
+
+        const t_line = gsap.timeline({
+            delay: 0.6
+        })
+
+        t_line
+            .to('.hero-content', {
+                opacity: 1,
+                y: 0,
+                ease: 'power1.inOut'
+            })
+            .to('.hero-text-scroll', {
+                duration: 0.7,
+                clipPath: 'polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)',
+                ease: 'circ.out'
+            })
+            .from(titleSplit.chars, {
+                yPercent: 100,
+                stagger: 0.02,
+                ease: 'power1.inOut',
+            })
+
+
+        const heroTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.hero-container',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        })
+
+        heroTl.to('.hero-container', {
+            rotate: 8,
+            scale: 0.85,
+            yPercent:30,
+            ease: 'power1.inOut'
+        })
+
+    }, [])
+
+
     return (
         <section className='bg-main-bg'>
             <div className='hero-container'>
@@ -9,7 +59,7 @@ const Hero = () => {
                     alt="static-img"
                     className='absolute bottom-0 left-1/2 -translate-x-1/2 object-auto '
                 />
-                <div className="hero-content">
+                <div className="hero-content opacity-0">
                     <div className='overflow-hidden'>
                         <h1 className="hero-title">Freaking Delicious</h1>
                     </div>
