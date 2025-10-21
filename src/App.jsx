f0ab3411c8b23e7319e507153587245a45cc7752
Fariya <fariya.webdev@gmail.com>
2025-10-21 14:56:08 +0600

    feat: smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import { ScrollTrigger } from "gsap/all"
+import { ScrollSmoother, ScrollTrigger } from "gsap/all"
 import Navbar from "./Components/Navbar"
 import Hero from "./Sections/Hero"
 import gsap from "gsap"
 import Texts from "./Sections/Texts"
+import Flavors from "./Sections/Flavors"
+import { useGSAP } from "@gsap/react"
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
 
 function App() {
 
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 2,
+      effects: true,
+    })
+  })
+
   return (
     <>
-      <Navbar/>
-      <Hero/>
-      <Texts/>
-      <div className="min-h-dvh z-50 overflow-hidden relative bg-red"></div>
+      <Navbar />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <Hero />
+          <Texts />
+          <Flavors />
+          <div className="min-h-dvh z-50 overflow-hidden relative bg-red"></div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/Sections/Flavors.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Flavors = () => {
+    return (
+        <section className='flavor-section'>
+            <div>
+                
+            </div>
+            
+        </section>
+    );
+};
+
+export default Flavors;
