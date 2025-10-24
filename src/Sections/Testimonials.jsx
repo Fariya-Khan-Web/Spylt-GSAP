a32f28aab7eb865bb65f0aa1edc583da14b7dcf2
Fariya <fariya.webdev@gmail.com>
2025-10-24 23:10:58 +0600

    fix: reveal time

--- src/Sections/Testimonials.jsx
+++ src/Sections/Testimonials.jsx
@@ -54,8 +54,8 @@ const Testimonials = () => {
 
         jumpinTL.from('.pin-box div', {
             yPercent: 200,
-            stagger: isMobile ? 8000 : 3.5,
-            duration: isMobile ? 8000 : 10,
+            stagger: isMobile ? 12000 : 3.5,
+            duration: isMobile ? 17000 : 10,
             ease: 'power1.out'
         })
 
